5864693ced35cf412e049c157681f8f057a29381
Theerthagiri Soundararajan <49114091+giritheerth@users.noreply.github.com>
2024-12-19 17:59:00 +1100

    info

--- src/main/java/com/learn/dependencyInjection/DependencyInjectionApplication.java
+++ src/main/java/com/learn/dependencyInjection/DependencyInjectionApplication.java
@@ -3,7 +3,7 @@ package com.learn.dependencyInjection;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
+//import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class DependencyInjectionApplication {
